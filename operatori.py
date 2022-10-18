5e0a4fccd7ecfa183277acd0ed15300ad97cc341
diana253 <115529967+diana253@users.noreply.github.com>
2022-10-18 09:30:30 +0300

    Fisierele initiale

---
+++ operatori.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def sumanumere():
+    print('introduceti primul numar, a=')
+    a = int(input())
+    print('numarul introdus a fost', a)
+
+    print('introduceti al doilea numar, b=')
+    b = int(input())
+    print('numarul introdus a fost', b)
+    print('suma numerelor a si b este a+b, adica', a+b)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    sumanumere()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
